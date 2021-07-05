3990ed55394c8c1676996a781ff7f174e155ad3d
Diego Arenas <darenasc@gmail.com>
2021-07-05 02:02:43 +0100

    snowflake sql added

---
+++ src/aeda/sql_scripts/snowflake/snowflake.sql
@@ -0,0 +1,82 @@
+create database if not exists aeda_metadata;
+
+use aeda_metadata;
+
+CREATE TABLE columns (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , COLUMN_NAME NVARCHAR(255)
+      , ORDINAL_POSITION INT
+      , DATA_TYPE NVARCHAR(255));
+      
+CREATE TABLE tables (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , N_COLUMNS INT
+      , N_ROWS INT);
+
+CREATE TABLE uniques (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , COLUMN_NAME NVARCHAR(255)
+      , ORDINAL_POSITION INT
+      , DATA_TYPE NVARCHAR(255)
+      , DISTINCT_VALUES INT
+      , NULL_VALUES INT);
+
+CREATE TABLE data_values (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , COLUMN_NAME NVARCHAR(255)
+      , DATA_VALUE NVARCHAR(255)
+      , FREQUENCY_NUMBER INT
+      , FREQUENCY_PERCENTAGE FLOAT);
+
+CREATE TABLE dates (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , COLUMN_NAME NVARCHAR(255)
+      , DATA_VALUE NVARCHAR(255)
+      , FREQUENCY_NUMBER INT
+      , FREQUENCY_PERCENTAGE FLOAT);
+
+CREATE TABLE stats (SERVER_NAME NVARCHAR(255)
+      , TABLE_CATALOG NVARCHAR(255)
+      , TABLE_SCHEMA NVARCHAR(255)
+      , TABLE_NAME NVARCHAR(255)
+      , COLUMN_NAME NVARCHAR(255)
+      , AVG FLOAT
+      , STDEV FLOAT
+      , "VAR" FLOAT
+      , SUM FLOAT
+      , MAX FLOAT
+      , MIN FLOAT
+      , "RANGE" FLOAT
+      , P01 FLOAT
+      , P025 FLOAT
+      , P05 FLOAT
+      , P10 FLOAT
+      , Q1 FLOAT
+      , Q2 FLOAT
+      , Q3 FLOAT
+      , P90 FLOAT
+      , P95 FLOAT
+      , P975 FLOAT
+      , P99 FLOAT
+      , IQR FLOAT);
+
+
+CREATE VIEW servers AS 
+select      server_name
+            , table_catalog 
+            , table_schema 
+            , count(distinct table_name) as n_tables
+            , sum(n_columns) as n_columns 
+            , sum(n_rows) as n_rows 
+from tables
+group by server_name, table_catalog , table_schema;
