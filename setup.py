36a860ebe35d130504d19992afb56e29f23d83ac
Diego Arenas <darenasc@gmail.com>
2021-08-02 09:39:37 +0100

    setup formatted

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'aeda',
-    description = 'Automated Exploratory Data Analysis',
-    packages = find_packages(where='src'),
-    package_dir = {"":"src"},
-    version = '0.0.1',
-    author = 'Diego Arenas',
-    author_email = 'darenasc@gmail.com'
+    name="aeda",
+    description="Automated Exploratory Data Analysis",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    version="0.0.1",
+    author="Diego Arenas",
+    author_email="darenasc@gmail.com",
 )
